6156e409d515699bf925f7a251548acef118dc40
Dom Christie <christiedom@gmail.com>
2020-07-14 11:34:34 -0700

    Add mobile

---
+++ react/outline/Mobile.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+function MobileIcon(props) {
+  return <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" {...props}><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 17h.01M10.5 4h3M17 5v14a2 2 0 01-2 2H9a2 2 0 01-2-2V5a2 2 0 012-2h6a2 2 0 012 2z" /></svg>;
+}
+
+export default MobileIcon;
---
+++ vue/outline/Mobile.js
@@ -0,0 +1,16 @@
+import { createVNode as _createVNode, openBlock as _openBlock, createBlock as _createBlock } from "vue"
+
+export default function render(_ctx, _cache) {
+return (_openBlock(), _createBlock("svg", {
+  fill: "none",
+  viewBox: "0 0 24 24",
+  stroke: "currentColor"
+}, [
+  _createVNode("path", {
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
+    "stroke-width": "2",
+    d: "M12 17h.01M10.5 4h3M17 5v14a2 2 0 01-2 2H9a2 2 0 01-2-2V5a2 2 0 012-2h6a2 2 0 012 2z"
+  })
+]))
+}
